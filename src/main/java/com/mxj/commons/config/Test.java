dcfde32f6b544f21d6ed60aae556d5f11427f208
reflyhight <jh624haima@126.com>
2017-11-18 02:18:46 +0800

    测试类

---
+++ src/main/java/com/mxj/commons/config/Test.java
@@ -0,0 +1,39 @@
+package com.mxj.commons.config;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Properties;
+
+public class Test {
+	
+	public static void main(String[] args) {
+		/*BOOLEAN, STRING, INT, SHORT, LONG, DOUBLE, LIST, CLASS,CHAR*/
+		
+		Properties properties=new Properties();
+		try {
+			properties.load(Test.class.getClassLoader().getResourceAsStream("spider.properties"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+//		SpiderConfig config= new SpiderConfig(properties);
+		SpiderConfig config= new SpiderConfig();
+		
+		
+		Boolean boolen_test = config.getBoolen(ConfigDefine.BOOLEN_TEST);
+		String string_test = config.getString(ConfigDefine.STRING_TEST);
+		Integer int_test = config.getInt(ConfigDefine.INT_TEST);
+		Short short_test = config.getShort(ConfigDefine.SHORT_TEST);
+		Long long_test = config.getLong(ConfigDefine.LONG_TEST);
+		Double double_test = config.getDouble(ConfigDefine.DOUBLE_TEST);
+		List<String> list_test = config.getList(ConfigDefine.LIST_TEST);
+		Class<?> claz = config.getClass(ConfigDefine.CLASS_TEST);
+		Character char_test = config.getChar(ConfigDefine.CHAR_TEST);
+		
+		System.out.println("00");
+		
+	}
+
+}
